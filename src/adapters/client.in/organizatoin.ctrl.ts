fd62df956bba58d8d2bfc7e3580c8ecd03757201
EN Chu <chuiantw1212@gmail.com>
2025-01-25 22:37:39 +0800

    wip

--- src/adapters/client.in/organizatoin.ctrl.ts
+++ src/adapters/client.in/organizatoin.ctrl.ts
@@ -2,47 +2,62 @@ import { memoryUsage } from 'node:process'
 import AccessGlobalService from '../../entities/app'
 import { Elysia, } from 'elysia'
 const router = new Elysia()
-// router.post('/organization', 'World!!')
-router.get('/organization', async () => {
-    const memoryUsageInMB: ReturnType<typeof memoryUsage> = {
-        rss: 0,
-        heapTotal: 0,
-        heapUsed: 0,
-        arrayBuffers: 0,
-        external: 0,
-    }
-    const currentMemoryUsage: any = memoryUsage()
-    for (const key in memoryUsageInMB) {
-        const mb: number = Math.floor(1024 * 1024)
-        const value: number = currentMemoryUsage[key]
-        const valueInMB: number = Math.floor(value / mb)
-        Object.assign(memoryUsageInMB, {
-            [key]: `${valueInMB.toLocaleString()}Mb`
-        })
-    }
-    return {
-        memoryUsage: memoryUsageInMB,
-        startupTime: AccessGlobalService.get('startupTime'),
-    }
+router.post('/organization', async ({ request }) => {
+  const organization = request.body
+  const { OrganizationService } = AccessGlobalService.locals
+  // const idToken = request.headers.authorization || ''
+
+  return {
+
+  }
 })
+
+/**
+ * var nodeRequestToWebstand = (req, abortController) => {
+  let _signal;
+  console.log('debug', "content-type" in req.headers)
+  const test = new Request(getUrl(req), {
+    method: req.method,
+    headers: req.headers,
+    get body() {
+      if (req.method === "GET" || req.method === "HEAD") return null;
+      return import_stream2.Readable.toWeb(req);
+    },
+    get signal() {
+      if (_signal) return _signal;
+      const controller = abortController ?? new AbortController();
+      _signal = controller.signal;
+      req.once("close", () => {
+        controller.abort();
+      });
+      return _signal;
+    },
+    // @ts-expect-error
+    duplex: "content-type" in req.headers ? "half" : "half"
+  });
+  
+  return test
+};
+ */
+
 router.put('/organization/logo', async () => {
-    try {
-        // const blob = request.body
-        const { OrganizationService } = AccessGlobalService.locals
-        // OrganizationService.storeLogo()
+  try {
+    // const blob = request.body
+    const { OrganizationService } = AccessGlobalService.locals
+    // OrganizationService.storeLogo()
 
-        return true
-    } catch (error) {
+    return true
+  } catch (error) {
 
-    }
-    // const idToken = request.headers.authorization || ''
-    // const user = await fastify.firebase.verifyIdToken(idToken)
-    // // Store Logo
-    // const company = await CompanyModel.getByAdminUid(user.uid)
-    // const publicUrl = await CompanyModel.storeLogo(company.id, blob)
-    // company.logo = publicUrl
-    // await CompanyModel.patchInfo(company)
-    // await JobModel.updateOrganization(company)
-    // res.code(200).send(publicUrl)
+  }
+  // const idToken = request.headers.authorization || ''
+  // const user = await fastify.firebase.verifyIdToken(idToken)
+  // // Store Logo
+  // const company = await CompanyModel.getByAdminUid(user.uid)
+  // const publicUrl = await CompanyModel.storeLogo(company.id, blob)
+  // company.logo = publicUrl
+  // await CompanyModel.patchInfo(company)
+  // await JobModel.updateOrganization(company)
+  // res.code(200).send(publicUrl)
 })
 export default router
