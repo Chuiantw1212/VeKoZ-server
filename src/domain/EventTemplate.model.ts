e93e076aa6833a3655d23c8930dd29ba4a23e142
EN Chu <chuiantw1212@gmail.com>
2025-02-12 11:39:38 +0800

    wip

--- src/domain/EventTemplate.model.ts
+++ src/domain/EventTemplate.model.ts
@@ -35,7 +35,6 @@ export default class EventTemplateModel extends FirestoreAdapter {
             count: {
                 range: [0, 1]
             },
-            orderBy: ['lastmod'],
         })
         return eventTemplates[0]
     }
