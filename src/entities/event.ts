f985d5acbbda21feb8ed3f6629668efda40ac80c
EN Chu <chuiantw1212@gmail.com>
2025-02-15 15:36:33 +0800

    fix

--- src/entities/event.ts
+++ src/entities/event.ts
@@ -1,18 +1,25 @@
 export interface IEvent {
     [key: string]: any
-    name?: string;
     uid?: string;
     id?: string; // doc id
+    // 搜尋&SEO用欄位
+    name?: string;
     description?: string,
     startDate?: any,
     endDate?: any,
-    dateDesignId?: string, // 從月曆拉動活動日期時，使用dateDesignId去更新對應套版設計的資料欄位
+    banner?: string,
+    logo?: string,
+    // 
     locationAddress?: string,
     locationName?: string,
     virtualLocationName?: string,
     virtualLocationUrl?: string,
+    // Email 附件夾帶
     attachments?: EventAttachment[],
     designIds?: string[],
+    isPublic?: boolean,
+    // 特殊，更新月曆使用
+    dateDesignId?: string, // 從月曆拉動活動日期時，使用dateDesignId去更新對應套版設計的資料欄位
 }
 
 export interface EventAttachment {
