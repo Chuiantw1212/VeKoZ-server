04598b730b3c3ecaa01df6a0fddd6ca792909135
EN Chu <chuiantw1212@gmail.com>
2025-02-25 17:06:28 +0800

    wip

--- src/adapters/client.in/user.ctrl.ts
+++ src/adapters/client.in/user.ctrl.ts
@@ -4,18 +4,6 @@ import { bearer } from '@elysiajs/bearer'
 import { IUser } from '../../entities/user'
 const router = new Elysia()
 router.use(bearer())
-    .get('/user', async ({ bearer }) => {
-        const { AuthService, UserService } = AccessGlobalService.locals
-        const userIdToken = await AuthService.verifyIdToken(bearer)
-        const user = await UserService.getUserByUid(userIdToken.uid)
-        return user
-    })
-    .get('/user/:seoName', async ({ params }) => {
-        const { UserService } = AccessGlobalService.locals
-        const { seoName } = params
-        const user = await UserService.getUserBySeoName(seoName)
-        return user
-    })
     .post('/user', async ({ bearer, request }) => {
         const { AuthService, UserService } = AccessGlobalService.locals
         const userIdToken = await AuthService.verifyIdToken(bearer)
@@ -29,6 +17,20 @@ router.use(bearer())
         const user = await request.json() as IUser
         const count = await UserService.setUser(userIdToken.uid, user)
         return count
+    })
+    // 不帶參數，只使用ID Token抓自己的資料
+    .get('/user', async ({ bearer }) => {
+        const { AuthService, UserService } = AccessGlobalService.locals
+        const userIdToken = await AuthService.verifyIdToken(bearer)
+        const user = await UserService.getUserByUid(userIdToken.uid)
+        return user
+    })
+    // 帶參數抓公開的使用者資料
+    .get('/user/:seoName', async ({ params }) => {
+        const { UserService } = AccessGlobalService.locals
+        const { seoName } = params
+        const user = await UserService.getUserBySeoName(seoName)
+        return user
     })
     .patch('/user/:id/seo-name', async ({ bearer, request }) => {
         const { AuthService, UserService } = AccessGlobalService.locals
